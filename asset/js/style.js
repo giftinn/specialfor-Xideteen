c62e937d224480f0cffda449563b60cb125b277a
giftin.id <calistanafisaa@gmail.com>
2026-02-21 22:39:50 +0700

    Update style.js

--- asset/js/style.js
+++ asset/js/style.js
@@ -3,7 +3,7 @@
       const c = setTimeout(() => {
         document.body.classList.remove("not-loaded");
 
-        const titles = ('I Love You, Athan').split('');
+        const titles = ('I\'m sorry, Xideteen').split('');
         const titleElement = document.getElementById('title');
         let index = 0;
 
@@ -21,7 +21,7 @@
           } else {
             // Setelah teks utama selesai, tambahkan subtitle kecil
             const subtitle = document.createElement('p');
-            subtitle.innerText = "Thank you for loving me with all your lovee – ilysb bb!<33";
+            subtitle.innerText = "Hi xideteen first of all, i'm deeply sorry soal kejadian kemarin. And thanks a lot karna mau menerima gua kembali balik kesini, tolong liat @martinfenomal ya gua punya hal kecil dan permintaan maaf disana";
             subtitle.style.fontSize = '20px'; // Ukuran font lebih kecil
             subtitle.style.marginTop = '2px'; // Spasi antara teks utama dan subtitle
             subtitle.style.color = '#fff'; // Warna abu-abu untuk teks subtitle
